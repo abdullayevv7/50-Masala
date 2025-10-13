e238976773695b1ea6a78fb1647bc2144416d735
Farhod <zxcvbnmqazwsx287@gmail.com>
2025-10-13 16:27:23 +0500

    Kod yuklandi!

---
+++ main.py
@@ -0,0 +1,183 @@
+##1-masala
+# s = input("Xarid summasini kiriting: ")
+
+# if s.isdigit():
+#     summa = int(s)
+
+#     if summa < 50000:
+#         print("Chegirma yo'q")
+#     elif summa < 100000:
+#         print("5% chegirma")
+#     elif summa < 200000:
+#         print("10% chegirma")
+#     else:
+#         print("15% chegirma")
+
+
+##2-masala
+# color = input("Svetoforning rangini kiriting (qizil, sariq, yashil): ")
+# yol = input("Yo'l tirbandmi? (ha/yo'q): ")
+
+# if color == "qizil":
+#     print("To'xtang!")
+# elif color == "sariq":
+#     print("Tayyorlaning")
+# elif color == "yashil" and yol == "yo'q":
+#     print("Yuring")
+# elif color == "yashil" and yol == "ha":
+#     print("Kuting")
+
+
+##3-masala
+# vaqt = int(input("Vaqtni kiriting:"))
+
+# if 6 >= {vaqt} <= 12:
+#     print("Ertalabgi dori")
+# elif 12 >= {vaqt} <= 18:
+#     print("Kunduzgi dori")
+# elif 18 > {vaqt} <=24:
+#     print("Kechki dori")
+# else:
+#     print("Hozir dori ichish kerak emas!")
+
+
+##4-masala
+# temp = int(input("Hozirgi haroratni kiriting: "))
+
+# if temp < 0:
+#     print("Qalin palto, qo‘lqop kiying")
+# elif temp < 15:
+#     print("Jaket kiying")
+# elif temp < 25:
+#     print("Futbolka yetarli")
+# else:
+#     print("Yengil kiyim, soyabon oling")
+
+
+##5-masala
+# sinf = int(input("Sinfni kiritiing:"))
+# sumka_vesi = int(input("Sumka o'g'irligini kiriting: "))
+
+# if 1 < {sinf} <= 4 and {sumka_vesi} > 2:
+#     print("Og'ir, kamaytiring!")
+# elif 5 >= {sinf} < 9 and {sumka_vesi} > 4:
+#     print("Og'ir, kamaytiring")
+# else:
+#     print("Norlam")
+
+
+##6-masala
+# yosh = input("Bemor yoshini kiriting: ")
+# halot = input("Bemor holatini kiriting: (og'ir/oddiy): ")
+
+# if yosh < 10 or yosh > 70:
+#     print("Zudlik bilan!")
+# elif halot == "og'ir":
+#     print("1-soat ichida")
+# else:
+#     print("3-soat ichida")
+
+
+##7-masala
+# kun = input("Kun nomini kiriting (masalan: Dushanba, Seshanba.....): ").lower
+# masofa = float(input("Masofani kiriting (km): "))
+
+# ish_kuni = ['dushanba', 'seshanba', 'chorshanba', 'payshanba', 'juma']
+# dam_kuni = ['shanba', 'yakshanba']
+
+# if kun == dam_kuni:
+#     if masofa > 10:
+#         print(f"Summa: {(3600 * masofa) * 0.9} Sizga masofa 10kmdan oshganligi sababli 10% chegirma beriladi.")
+#     else:
+#         print(f"Summa: {3600  * masofa} so'm.")
+# elif kun == ish_kuni:
+#     if masofa > 10:
+#         print(f"Summa: {(3600 * masofa) * 0.9} Sizga masofa 10kmdan oshganligi sababli 10% chegirma beriladi.")
+#     else:
+#         print(f"Summa: {3000 * masofa} so'm.")
+# else:
+#     print("Qiymat xato kiritildi!")
+
+
+##8-masala
+
+
+
+##9-masala
+
+
+
+##10-masala
+
+
+##11-masala
+
+
+##12-masala
+
+
+##13-masala
+
+
+##14-masala
+
+
+
+##15-masala
+
+
+##16-masala
+
+
+##17-masala
+
+
+##18-masala
+
+
+##19-masala
+
+
+##20-masala
+
+
+##21-masala
+
+
+##22-masala
+
+
+##23-masala
+
+
+##1-masala
+
+
+##1-masala
+
+
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##1-masala
+##49-masala
+##50-masala
